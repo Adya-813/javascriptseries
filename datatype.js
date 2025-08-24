926dc6602e3713271191cffe3644de7f50364a4f
Adya-813 <mahapatradyasha2003@gmail.com>
2025-08-24 19:06:16 +0530

    Adding datatypes files

---
+++ datatype.js
@@ -0,0 +1,18 @@
+let score=33
+console.log(typeof(score));
+console.log(typeof score);
+let score1="33abc"
+console.log(typeof(score1));
+console.log(typeof score1);
+let valueInNumber1 = Number(score1)
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+let isLoggedIn="Adyasha" //empty string the op will be false
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1=true;0=false
+//"adyasha"=true
+let number = 32
+let stringNumber = String(number)
+console.log(typeof stringNumber);
+console.log(stringNumber);
